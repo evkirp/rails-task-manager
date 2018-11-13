be39b65aff0991e796fc66752ddc29a7c029f923
Evgeniya Kirpicheva <jenny_blz@yahoo.com>
2018-11-13 16:48:18 +0100

    created controller and view for tasks index, fixed task default value

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,7 @@
 class Task < ApplicationRecord
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.completed = false if completed.nil?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_123557) do
+ActiveRecord::Schema.define(version: 2018_11_13_151623) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
+    t.boolean "completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
